e152b2fa6b5a884f7d9344848bc16464bbe70595
Blake <blake.lieberman@gmail.com>
2015-02-02 16:36:26 -0800

    adding module and controller

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+angular.module('myApp',[])
+  .controller('MyCtrl', function($scope){
+    $scope.male = 'male name';
+    $scope.dirty = 'dirty task';
+    $scope.obnox = 'obnoxious celebrity';
+    $scope.job = 'job title';
+    $scope.celeb= 'celebrity';
+    $scope.huge = 'huge number';
+    $scope.tedious = 'tedious task';
+    $scope.useless = 'useless skill';
+    $scope.adjective = 'adjective';
+  });
